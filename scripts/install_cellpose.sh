3140f923aba64c9069f3955614e7a0d3ba01ef5e
Stephen Cross <stephencross86@gmail.com>
2025-07-21 12:17:47 +0100

    Update install_cellpose.sh

--- scripts/install_cellpose.sh
+++ scripts/install_cellpose.sh
@@ -1,34 +1,13 @@
 #!/bin/bash
 
-cd ${WORK}
+# Installing Conda if necessary
+wget https://raw.githubusercontent.com/wbif-bristol/HPC-scripts/refs/heads/main/scripts/install_conda.sh -O ${WORK}/install_conda.sh
+chmod +x ${WORK}/install_conda.sh
+${WORK}/install_conda.sh
+rm ${WORK}/install_conda.sh
 
-# Check if we need to install conda
-if command -v conda >/dev/null 2>&1; then
-    # Conda is installed and recognised
-    echo "Conda is already available"
-else
-    if [ -f "${WORK}/initConda.sh" ]; then
-        # Conda initialisation script exists
-        echo "Conda is already available"
-    else
-        echo "Installing Conda"
-
-        # Installing Conda
-        wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-        chmod u+x Miniconda3-latest-Linux-x86_64.sh
-        ${WORK}/Miniconda3-latest-Linux-x86_64.sh -p ${WORK}/miniconda3     
-        rm ${WORK}/Miniconda3-latest-Linux-x86_64.sh
-
-        # Getting script to initialise Conda
-        wget https://raw.githubusercontent.com/wbif-bristol/HPC-scripts/refs/heads/main/scripts/init_conda.sh
-        chmod +x init_conda.sh
-
-        # Creating Cellpose environment
-        ${WORK}/initConda.sh
-        conda create --name cellpose python=3.10
-        conda activate cellpose
-        pip install cellpose
-
-    fi
-fi
-
+# Creating Cellpose environment
+${WORK}/initConda.sh
+conda create --name cellpose python=3.10 -y
+conda activate cellpose
+pip install cellpose
