c7b9723811c87f22acd93a756fd92f93ee8c2dd6
Stephen Cross <stephencross86@gmail.com>
2025-07-21 12:19:30 +0100

    Update install_conda.sh

--- scripts/install_conda.sh
+++ scripts/install_conda.sh
@@ -5,21 +5,16 @@ if command -v conda >/dev/null 2>&1; then
     # Conda is installed and recognised
     echo "Conda is already available"
 else
-    if [ -f "${WORK}/initConda.sh" ]; then
-        # Conda initialisation script exists
-        echo "Conda is already available"
-    else
-        echo "Installing Conda"
+    echo "Installing Conda"
 
-        # Installing Conda
-        wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ${WORK}/Miniconda3-latest-Linux-x86_64.sh
-        chmod u+x ${WORK}/Miniconda3-latest-Linux-x86_64.sh
-        ${WORK}/Miniconda3-latest-Linux-x86_64.sh -p ${WORK}/miniconda3     
-        rm ${WORK}/Miniconda3-latest-Linux-x86_64.sh
+    # Installing Conda
+    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ${WORK}/Miniconda3-latest-Linux-x86_64.sh
+    chmod u+x ${WORK}/Miniconda3-latest-Linux-x86_64.sh
+    ${WORK}/Miniconda3-latest-Linux-x86_64.sh -p ${WORK}/miniconda3     
+    rm ${WORK}/Miniconda3-latest-Linux-x86_64.sh
 
-        # Getting script to initialise Conda
-        # wget https://raw.githubusercontent.com/wbif-bristol/HPC-scripts/refs/heads/main/scripts/init_conda.sh -O ${WORK}/init_conda.sh
-        # chmod u+x ${WORK}/init_conda.sh
+    # Getting script to initialise Conda
+    wget https://raw.githubusercontent.com/wbif-bristol/HPC-scripts/refs/heads/main/scripts/init_conda.sh -O ${WORK}/init_conda.sh
+    chmod u+x ${WORK}/init_conda.sh
         
-    fi
 fi
