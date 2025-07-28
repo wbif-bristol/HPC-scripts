ac0b0dcfada4813835d14791abad25da0e242924
Stephen Cross <stephencross86@gmail.com>
2025-07-28 16:01:04 +0100

    Added ability to select Cellpose version

--- scripts/install_cellpose.sh
+++ scripts/install_cellpose.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+version
+
 # Installing Conda if necessary
 wget https://raw.githubusercontent.com/wbif-bristol/HPC-scripts/refs/heads/main/scripts/install_conda.sh -O ${WORK}/install_conda.sh
 chmod +x ${WORK}/install_conda.sh
@@ -13,7 +15,15 @@ if [ -d "${WORK}/miniconda3/envs/cellpose" ]; then
 else
     conda create --name cellpose python=3.10 -y
     conda activate cellpose
-    pip install cellpose
+
+    if [ "$1" == "" ]; then
+        echo "Using latest Cellpose"
+        pip install cellpose
+    else
+        echo "Using Cellpose version $1"
+        pip install cellpose==$1ß
+    fi
+
     pip uninstall torch -y
     pip install torch torchvision --index-url https://download.pytorch.org/whl/cu124
 
--- scripts/install_cellpose_and_fiji.sh
+++ scripts/install_cellpose_and_fiji.sh
@@ -4,7 +4,7 @@
 wget https://raw.githubusercontent.com/wbif-bristol/HPC-scripts/refs/heads/main/scripts/install_cellpose.sh -O ${WORK}/install_cellpose.sh
 chmod +x ${WORK}/install_cellpose.sh
 ${WORK}/install_cellpose.sh
-rm ${WORK}/install_cellpose.sh
+rm ${WORK}/install_cellpose.sh $1
 
 wget https://raw.githubusercontent.com/wbif-bristol/HPC-scripts/refs/heads/main/scripts/install_fiji.sh -O ${WORK}/install_fiji.sh
 chmod +x ${WORK}/install_fiji.sh
