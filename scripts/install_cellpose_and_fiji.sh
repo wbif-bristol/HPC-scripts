6be326f68f087667ddca162123fb532de18c3b69
Stephen Cross <stephencross86@gmail.com>
2025-07-28 16:12:39 +0100

    Fix for installation

--- scripts/install_cellpose_and_fiji.sh
+++ scripts/install_cellpose_and_fiji.sh
@@ -3,8 +3,8 @@
 # Creating Cellpose environment
 wget https://raw.githubusercontent.com/wbif-bristol/HPC-scripts/refs/heads/main/scripts/install_cellpose.sh -O ${WORK}/install_cellpose.sh
 chmod +x ${WORK}/install_cellpose.sh
-${WORK}/install_cellpose.sh
-rm ${WORK}/install_cellpose.sh $1
+${WORK}/install_cellpose.sh $1
+rm ${WORK}/install_cellpose.sh
 
 wget https://raw.githubusercontent.com/wbif-bristol/HPC-scripts/refs/heads/main/scripts/install_fiji.sh -O ${WORK}/install_fiji.sh
 chmod +x ${WORK}/install_fiji.sh
